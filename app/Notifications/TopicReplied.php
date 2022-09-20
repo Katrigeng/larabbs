271bdf5042121cd6a0161289229274c251edfbb7
katrigeng <453875798@qq.com>
2022-09-21 00:01:02 +0800

    消息通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -51,7 +51,20 @@ class TopicReplied extends Notification
 
     public function toDatabase($notifiable)
     {
+        $topic = $this->reply->topic;
+        $link =  $topic->link(['#reply' . $this->reply->id]);
 
+        // 存入数据库里的数据
+        return [
+            'reply_id' => $this->reply->id,
+            'reply_content' => $this->reply->content,
+            'user_id' => $this->reply->user->id,
+            'user_name' => $this->reply->user->name,
+            'user_avatar' => $this->reply->user->avatar,
+            'topic_link' => $link,
+            'topic_id' => $topic->id,
+            'topic_title' => $topic->title,
+        ];
     }
 
     /**
